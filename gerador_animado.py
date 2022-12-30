7be880aed82e790ce86583dea97aa81ea8cf1f23
leoobrabo <leonardomotta2010@hotmail.com>
2022-12-30 16:57:20 -0300

    my first commit

--- gerador_animado.py
+++ gerador_animado.py
@@ -3,9 +3,9 @@ from amzqr import amzqr
 amzqr.run(
     'https://bit.ly/mapa-de-salas-ft',
     level='H',
-    picture='ft.png',
+    picture='image.png',
     colorized=True,
-    save_name='qrcode1.png',
+    save_name='qrcode.png',
     save_dir='./',
 
 )
--- main.py
+++
@@ -1,9 +0,0 @@
-import qrcode
-# Link for website
-#input_data = "https://towardsdatascience.com/face-detection-in-10-lines-for-beginners-1787aa1d9127"
-
-#Creating an instance of qrcode
-data = print(input('Digite o link para o qrcode:'))
-img = qrcode.make(str(data))
-#Saving the image
-img.save('qrcode.png')
